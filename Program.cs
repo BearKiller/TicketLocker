a9f351860df79e4c595778cba02bc2b05b8c1320
BearKiller <kenneth.byrkett@gmail.com>
2024-03-25 15:44:21 -0500

    [1.7] Created menu to choose which file to read.  - Only bug tickets may be parsed

--- Program.cs
+++ Program.cs
@@ -1,18 +1,21 @@
 using Helper;
 using NLog;
 
-string file = "Tickets.csv";
+string bugPath = "Tickets.csv";
+string enhancementPath = "Enhancements.csv";
+string taskPath = "Tasks.csv";
 string option; 
 string path = Directory.GetCurrentDirectory() + $"{Path.DirectorySeparatorChar}nlog.config";
 var logger = LogManager.LoadConfiguration(path).GetCurrentClassLogger();
 logger.Info("Program started");
 
+TicketFile bugFile = new TicketFile(bugPath);
+
 do{
-    Console.Clear();
-    Console.WriteLine("1.) Read tickets from file.");
-    Console.WriteLine("2.) Create new ticket file.");
-    Console.WriteLine("3.) Append new ticket to file.");
-    Console.WriteLine("4.) Exit program.");
+    //Console.Clear();
+    Console.WriteLine("\n1.) Read tickets from file.");
+    Console.WriteLine("2.) Append new ticket to file.");
+    Console.WriteLine("3.) Exit program.");
     Console.Write("Enter option > ");
     option = Console.ReadLine();
 
@@ -20,123 +23,65 @@ do{
 
         //Reads data from the Tickets.csv file
         case "1":
-        if (File.Exists(file)) {
-            StreamReader sr = new StreamReader("Tickets.csv");
-            while (!sr.EndOfStream) {
-                string? line = sr.ReadLine();
-                string[] arr = line.Split(',');
-                string watchersSep = line.Split(',').Last();
-                string[] watchersArr = watchersSep.Split('|');
-                string watchers = String.Join(", ", watchersArr);
-                Console.WriteLine("\nTicket ID: {0} \n Summary: {1} \n Status: {2} \n Priority: {3} \n Submitter: {4} \n Watching: {5} \n", arr[0], arr[1], arr[2], arr[3], arr[4], watchers);
-            } sr.Close();
+        logger.Info("User choice: \"1\"");
+        string fileOption;
+        Console.Clear();
+        Console.WriteLine("Read what file?");
+        Console.WriteLine("1.Bugs | 2.Enhancements | 3.Tasks");
+        fileOption = Console.ReadLine();
+        switch(fileOption) {
 
-        }
-        else {
-            Console.WriteLine("File does not exist.");
-        }
-            break;
-
-        //Creates a new ticket file
-        case "2":
-        if (!File.Exists(file)) {
-            CreateTicket(file);
-            
-        }
-        else {
-            Console.Write("File already exists. Overwrite? (Y/N) > ");
-            string overwrite = Console.ReadLine().ToUpper();
-            if (overwrite == "Y") {
-                CreateTicket(file);
-                
-            }
-            else {
+            //Reads from the Tickets.csv file
+            case "1":
+            if (File.Exists(bugPath)) {
+                foreach (Bug bug in bugFile.Bugs) {
+                    Console.Write($"\nID: {bug.ticketID} - ");
+                    Console.WriteLine($"Summary: {bug.summary}");
+                    Console.WriteLine($"Status: {bug.status}");
+                    Console.Write($"Priority: {bug.priority} - ");
+                    Console.Write($"Submitter: {bug.submitter} - ");
+                    Console.WriteLine($"Assigned: {bug.assigned}");
+                    Console.WriteLine($"Watchers: {string.Join(", ", bug.watching)}");
+                    Console.WriteLine($"Severity: {bug.severity}");
+                }
                 break;
+            } else {
+            Console.WriteLine("File does not exist.");
             }
-        }
             break;
+
+            //Reads from the Enhancements.csv file
+            case "2":
+            if (File.Exists(enhancementPath)) {
+                StreamReader sr = new StreamReader("Enhancements.csv");
+                while (!sr.EndOfStream) {
+                    
+                } sr.Close();
+                break;
+            } else {
+            Console.WriteLine("File does not exist.");
+            }
+            break;
+
+            //Reads from the Tasks.csv file
+            case "3":
+            if (File.Exists(taskPath)) {
+                StreamReader sr = new StreamReader("Tasks.csv");
+                while (!sr.EndOfStream) {
+                    
+                } sr.Close();
+                break;
+            } else {
+            Console.WriteLine("File does not exist.");
+            }
+            break;
+        }
+        break;
 
         //Adds new data to the existing Tickets.csv file
-        case "3":
-        if (!File.Exists(file)) {
-            Console.WriteLine("Please create file first.");
-        } else {
-            AppendTicket(file);
-        }
-            break;
+        case "2":
+        break;
     }
 
-} while (option == "1" || option == "2" || option == "3");
-
-
-
-static void CreateTicket(string file) {
-
-    Console.Clear();
-    string summary = Inputs.GetString("Enter a summary.");
-    string status = Inputs.GetString("Enter the status of the ticket.");
-    string priority = Inputs.GetString("Enter priority of the ticket.");
-    string submitter = Inputs.GetString("Enter submitter.");
-    string assigned = Inputs.GetString("Enter who this ticket is assigned to.");
-    string watchers = Inputs.GetString("Enter any watchers (separated by commas)");
-    string[] watcher = watchers.Split(", ");
-    string watchersJoined = String.Join("|", watcher);
-
-    Tickets newTicket = new Tickets(summary, status, priority, submitter, assigned, watchersJoined);
-
-    StreamWriter sw = new StreamWriter(file);
-    sw.WriteLine(newTicket.ID + "," + newTicket.Summary + "," + newTicket.Status + "," + newTicket.Priority + "," + newTicket.Submitter + "," + newTicket.Assigned + "," + newTicket.Watchers);
-    sw.Close();
-
-}
-
-
-
-static void AppendTicket(string file) {
-
-    Console.Clear();
-    string summary = Inputs.GetString("Enter a summary.");
-    string status = Inputs.GetString("Enter the status of the ticket.");
-    string priority = Inputs.GetString("Enter priority of the ticket.");
-    string submitter = Inputs.GetString("Enter submitter.");
-    string assigned = Inputs.GetString("Enter who this ticket is assigned to.");
-    string watchers = Inputs.GetString("Enter any watchers (separated by commas)");
-    string[] watcher = watchers.Split(", ");
-    string watchersJoined = String.Join("|", watcher);
-
-    Tickets newTicket = new Tickets(summary, status, priority, submitter, assigned, watchersJoined);
-
-    StreamWriter sw = new StreamWriter(file, true);
-    sw.WriteLine(newTicket.ID + "," + newTicket.Summary + "," + newTicket.Status + "," + newTicket.Priority + "," + newTicket.Submitter + "," + newTicket.Assigned + "," + newTicket.Watchers);
-    sw.Close();
-}
-
-
-
-public class Tickets {
-    public int ID { get; set; }
-    public string Summary { get; set; }
-    public string Status { get; set; }
-    public string Priority { get; set; }
-    public string Submitter { get; set; }
-    public string Assigned { get; set; }
-    public string Watchers { get; set; }
-
-    public Tickets(string summary, string status, string priority, string submitter, string assigned, string watchers) {
-    
-        int i = 1;
-        StreamReader sr = new StreamReader("Tickets.csv");
-        while (!sr.EndOfStream) {
-            i += 1;
-        } sr.Close();
-
-        ID = i;
-        Summary = summary;
-        Status = status;
-        Priority = priority;
-        Submitter = submitter;
-        Assigned = assigned;
-        Watchers = watchers;
-    }
-
-}
+} while (option == "1" || option == "2");
+logger.Info("Program ended");
