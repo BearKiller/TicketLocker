0c92da9ea55d0d502545d6f7b19f4fb75fd0e1cb
BearKiller <kenneth.byrkett@gmail.com>
2024-03-26 14:23:22 -0500

    [1.10] User can now append tickets to the corresponding ticket files  - Ticket lists now update so user can view changes in the same instance

--- Program.cs
+++ Program.cs
@@ -9,12 +9,14 @@ string path = Directory.GetCurrentDirectory() + $"{Path.DirectorySeparatorChar}n
 var logger = LogManager.LoadConfiguration(path).GetCurrentClassLogger();
 logger.Info("Program started");
 
-TicketFile bugFile = new TicketFile(bugPath);
-EnhancementFile enhancementFile = new EnhancementFile(enhancementPath);
-TaskFile taskFile = new TaskFile(taskPath);
+
 
 do{
     //Console.Clear();
+    TicketFile bugFile = new TicketFile(bugPath);
+    EnhancementFile enhancementFile = new EnhancementFile(enhancementPath);
+    TaskFile taskFile = new TaskFile(taskPath);
+
     Console.WriteLine("\n1.) Read tickets from file.");
     Console.WriteLine("2.) Append new ticket to file.");
     Console.WriteLine("3.) Exit program.");
@@ -96,10 +98,150 @@ do{
         }
         break;
 
-        //Adds new data to the existing Tickets.csv file
+        //Adds new data to the existing files
         case "2":
+        logger.Info("User choice: \"2\"");
+        Console.Clear();
+        Console.WriteLine("Add what type of ticket?");
+        Console.WriteLine("1.Bugs | 2.Enhancements | 3.Tasks");
+        fileOption = Console.ReadLine();
+        switch(fileOption) {
+
+            //Adds bug tickets to the Tickets.csv file
+            case "1":
+            logger.Info("User choice: Add Bug ticket");
+            if (File.Exists(bugPath)) {
+                Bug newBug = new Bug();
+
+                Console.WriteLine("Enter bug summary:");
+                newBug.summary = Console.ReadLine();
+
+                Console.WriteLine("Enter bug status:");
+                newBug.status = Console.ReadLine();
+
+                Console.WriteLine("Enter bug priority:");
+                newBug.priority = Console.ReadLine();
+
+                Console.WriteLine("Enter bug submitter:");
+                newBug.submitter = Console.ReadLine();
+
+                Console.WriteLine("Enter user assigned to this ticket:");
+                newBug.assigned = Console.ReadLine();
+
+                Console.WriteLine("Enter any watchers (type 'done' to finish):");
+                newBug.watching = new List<string>();
+                string watcherInput;
+                do {
+                    watcherInput = Console.ReadLine();
+                    if (watcherInput.ToLower() != "done")
+                        newBug.watching.Add(watcherInput);
+                } while (watcherInput.ToLower() != "done");
+
+                Console.WriteLine("Enter the severity of the bug:");
+                newBug.severity = Console.ReadLine();
+                
+                bugFile.AddBugTicket(newBug);
+
+            } else {
+                logger.Warn("Tickets.csv is missing.");
+            }
+            break;
+
+            //Adds enhancement tickets to the Enhancements.csv file
+            case "2":
+            logger.Info("User choice: Add Enhancement ticket");
+            if (File.Exists(enhancementPath)) {
+                Enhancement newEnhancement = new Enhancement();
+
+                Console.WriteLine("Enter bug summary:");
+                newEnhancement.summary = Console.ReadLine();
+
+                Console.WriteLine("Enter bug status:");
+                newEnhancement.status = Console.ReadLine();
+
+                Console.WriteLine("Enter bug priority:");
+                newEnhancement.priority = Console.ReadLine();
+
+                Console.WriteLine("Enter bug submitter:");
+                newEnhancement.submitter = Console.ReadLine();
+
+                Console.WriteLine("Enter user assigned to this ticket:");
+                newEnhancement.assigned = Console.ReadLine();
+
+                Console.WriteLine("Enter any watchers (type 'done' to finish):");
+                newEnhancement.watching = new List<string>();
+                string watcherInput;
+                do {
+                    watcherInput = Console.ReadLine();
+                    if (watcherInput.ToLower() != "done")
+                        newEnhancement.watching.Add(watcherInput);
+                } while (watcherInput.ToLower() != "done");
+
+                Console.WriteLine("Enter available software:");
+                newEnhancement.software = Console.ReadLine();
+                
+                Console.WriteLine("Enter cost:");
+                newEnhancement.cost = Console.ReadLine();
+
+                Console.WriteLine("Enter a reason:");
+                newEnhancement.reason = Console.ReadLine();
+
+                Console.WriteLine("Enter an estimate:");
+                newEnhancement.estimate = Console.ReadLine();
+
+                enhancementFile.AddEnhancementTicket(newEnhancement);
+
+            } else {
+                logger.Warn("Enhancements.csv is missing.");
+            }
+            break;
+
+            //Adds task tickets to the Tasks.csv file
+            case "3":
+            logger.Info("User choice: Add Task ticket");
+            if (File.Exists(taskPath)) {
+                Task newTask = new Task();
+
+                Console.WriteLine("Enter bug summary:");
+                newTask.summary = Console.ReadLine();
+
+                Console.WriteLine("Enter bug status:");
+                newTask.status = Console.ReadLine();
+
+                Console.WriteLine("Enter bug priority:");
+                newTask.priority = Console.ReadLine();
+
+                Console.WriteLine("Enter bug submitter:");
+                newTask.submitter = Console.ReadLine();
+
+                Console.WriteLine("Enter user assigned to this ticket:");
+                newTask.assigned = Console.ReadLine();
+
+                Console.WriteLine("Enter any watchers (type 'done' to finish):");
+                newTask.watching = new List<string>();
+                string watcherInput;
+                do {
+                    watcherInput = Console.ReadLine();
+                    if (watcherInput.ToLower() != "done")
+                        newTask.watching.Add(watcherInput);
+                } while (watcherInput.ToLower() != "done");
+
+                Console.WriteLine("Enter a project name:");
+                newTask.projectName = Console.ReadLine();
+                
+                Console.WriteLine("Enter a due date:");
+                newTask.dueDate = Console.ReadLine();
+
+                taskFile.AddTaskTicket(newTask);
+
+            } else {
+                logger.Warn("Tasks.csv is missing.");
+            }
+            break;
+        }
         break;
     }
+    
 
 } while (option == "1" || option == "2");
 logger.Info("Program ended");
